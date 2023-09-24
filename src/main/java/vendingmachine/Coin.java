4d4a69343eca9665a24a61d9a7037ff8354783d7
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-24 17:23:42 +0900

    feat(CoinRandomGenerator) : 자판기가 보유한 금액 만큼 무작위하게 동전 생성하는 메서드 구현

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,12 +1,19 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public enum Coin {
     COIN_500(500, 0),
     COIN_100(100, 0),
     COIN_50(50, 0),
     COIN_10(10, 0);
 
-    private final int amount;
+    final int amount;
     int count;
 
     Coin(final int amount, final int count) {
@@ -14,5 +21,10 @@ public enum Coin {
         this.count = count;
     }
 
-    // 추가 기능 구현
+    public static void addCount(int amount) {
+        Arrays.stream(values())
+                .filter(c -> c.amount == amount)
+                .findFirst()
+                .ifPresent(coin -> coin.count++);
+    }
 }
--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,14 +1,20 @@
 package vendingmachine.controller;
 
+import vendingmachine.Coin;
+import vendingmachine.domain.CoinRandomGenerator;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
+
+import java.util.List;
 
 public class VendingMachineController {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
+    private final CoinRandomGenerator coinGenerator = new CoinRandomGenerator();
 
     public void start() {
         outputView.printInputAmountVendingMachineHas();
-        inputView.readAmountVendingMachineHas();
+        int totalAmount = inputView.readAmountVendingMachineHas();
+        coinGenerator.generate(totalAmount);
     }
 }
