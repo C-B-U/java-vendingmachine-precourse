7ec27121b2739cae248229c06c649c8b60146eca
GaBaljaintheroom <junsu1222@naver.com>
2023-09-25 01:09:22 +0900

    feat: ProgressMessage 메시지 추가작성

--- src/main/java/vendingmachine/ProgressMessage.java
+++ src/main/java/vendingmachine/ProgressMessage.java
@@ -2,7 +2,8 @@ package vendingmachine;
 
 public enum ProgressMessage {
     INPUT_MACHINE_AMOUNT_HELD("자판기가 보유하고 있는 금액을 입력해 주세요."),
-    MACHINE_AMOUNT_HELD("자판기가 보유한 동전");
+    MACHINE_AMOUNT_HELD("자판기가 보유한 동전"),
+    INPUT_PRODUCTS_DETAIL("상품명과 가격, 수량을 입력해 주세요.");
 
     private final String message;
 
