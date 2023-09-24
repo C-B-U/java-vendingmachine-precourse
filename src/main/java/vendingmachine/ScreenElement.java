ab3488016fecbc9fb98c4461ddbcd62254276958
GaBaljaintheroom <junsu1222@naver.com>
2023-09-25 00:05:04 +0900

    feat: ScreenElement 생성

---
+++ src/main/java/vendingmachine/ScreenElement.java
@@ -0,0 +1,18 @@
+package vendingmachine;
+
+public enum ScreenElement {
+    COIN_UNIT("원"),
+    DIVISION(" - "),
+    COUNT_UNIT("개");
+
+    private final String value;
+
+    ScreenElement(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
