fb9b1182a0daadb6371d3f1a5144edf26bc05207
GaBaljaintheroom <junsu1222@naver.com>
2023-09-25 01:08:38 +0900

    feat: Product 생성

---
+++ src/main/java/vendingmachine/Product.java
@@ -0,0 +1,27 @@
+package vendingmachine;
+
+public class Product {
+
+    private static final int MIN_PRICE = 100;
+    private static final int COIN_UNIT = 10;
+    private final String name;
+    private final int price;
+    private final int amount;
+
+    public Product(String name, int price, int amount) {
+        validatePriceRule(price);
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    private void validatePriceRule(int price) {
+        if (isaBreakPriceRule(price)){
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_PRICE_ERROR.getMessage());
+        }
+    }
+
+    private static boolean isaBreakPriceRule(int price) {
+        return price < MIN_PRICE || price % COIN_UNIT != 0;
+    }
+}
